f8168fbdfcf1e2f78a7419d2d13322582ecf55d5
Eliezer <eliezerTerrero12@gmail.com>
2024-02-12 18:23:59 -0400

    Creando Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP1_EliezerTerrero.Models;
+
+namespace Parcial1_AP1_EliezerTerrero.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions options) :base(options) { }
+
+        public DbSet<Metas> Metas { get; set; }
+    }
+}
