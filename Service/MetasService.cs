f85c2a670df6434042b8d68c3e30ac521f8cd588
Eliezer <eliezerTerrero12@gmail.com>
2024-02-12 18:37:06 -0400

    Creando Metas Services

---
+++ Service/MetasService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_AP1_EliezerTerrero.DAL;
+using Parcial1_AP1_EliezerTerrero.Models;
+using System.Linq.Expressions;
+
+namespace Parcial1_AP1_EliezerTerrero.Service
+{
+    public class MetasService
+    {
+        protected readonly Contexto _contexto;
+
+        public MetasService(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<bool> save(Metas meta)
+        {
+            if (meta.MetaId == 0) 
+            {
+                _contexto.Metas.Add(meta);
+            }
+            else
+            {
+                _contexto.Metas.Remove(meta);
+            }
+
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var meta = _contexto.Metas.Find(id);
+            _contexto.Metas.Remove(meta);
+
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Metas?> firstIdAsync(int id)
+        {
+            return await _contexto.Metas.FirstOrDefaultAsync(p => p.MetaId == id);
+        }
+
+        public async Task<List<Metas>> GetMetas (Expression<Func<Metas, bool>> expression)
+        {
+            return await _contexto.Metas
+                .AsNoTracking()
+                .Where(expression)
+                .ToListAsync();
+        }
+    }
+}
