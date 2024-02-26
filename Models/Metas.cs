7a9cb318f24ceef69c7b01de85545f016a24d15d
eliezer terrero <eliezerterrero12@gmail.com>
2024-02-25 23:26:59 -0400

    #2 corrigiendo el monto maximo

--- Models/Metas.cs
+++ Models/Metas.cs
@@ -12,7 +12,7 @@ namespace Parcial1_AP1_EliezerTerrero.Models
         public DateTime Fecha { get; set; }
 
         //[Required(ErrorMessage = "El monto es requerido")]
-        [Range(1, int.MaxValue, ErrorMessage ="El monto es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage ="El monto es requerido")]
         public Decimal Monto { get; set; }
     }
 }
